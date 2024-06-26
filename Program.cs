8e4d19cb59348cc342574cd6ba52d3bc53fa9be3
Jahle <Jahle@ABLACKMAN>
2024-06-26 16:09:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Assignment_8._1
+{
+    class Program
+    {
+        static void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(arr, low, high);
+                QuickSort(arr, low, pivotIndex - 1);
+                QuickSort(arr, pivotIndex + 1, high);
+            }
+        }
+
+        static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, high);
+            return i + 1;
+        }
+
+        static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static void Main()
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            QuickSort(arr, 0, arr.Length - 1);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
